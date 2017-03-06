36ddc6cd3c2bee7da52401860b8ff50e041c3eb7
iegan <ije@umail.ucsb.edu>
2017-03-05 19:47:09 -0800

    Prints empty board successfully

--- gobang.py
+++ gobang.py
@@ -3,21 +3,27 @@ import sys
 import os
 import time
 import argparse
+import random
 
-#var inits
-board_size = None
+#consts
 DARK = 9
 LIGHT = 7
 EMPTY = 0
+
+#var inits
+board_size = None
 board = []
 PLAYER = None
 OPPONENT = None
-
-
+curr_turn = None
 
 #argument parsing
 def parse():
     global board_size
+    global curr_turn
+    global PLAYER
+    global OPPONENT
+
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-l', action = 'store_true', help = "this decides whether the player uses light or dark pieces")
@@ -32,9 +38,11 @@ def parse():
     if args.l:
         PLAYER = 7
         OPPONENT = 9
+        curr_turn = PLAYER
     else:
         OPPONENT = 7
         PLAYER = 9
+        curr_turn = OPPONENT
 
 
 def print_board():
@@ -72,10 +80,53 @@ def makeBoard(board_size):
             row.append(EMPTY)
         board.append(row)
 
+#check if win/draw program
+#read from stdin for move
+#pick random move
+
+def is_empty(column, row):
+    if board[row][column] == EMPTY:
+        return True
+    else:
+        return False
+
+def rand_move():
+    column = random.randint(0, board_size-1)
+    row = random.randint(0, board_size-1)
+    correct_move = is_empty(column, row)
+    while not correct_move:
+        column = random.randint(0, board_size-1)
+        row = random.randint(0, board_size-1)
+        correct_move = is_empty(column, row)
+    board[row][column] = curr_turn
+
+def read_move():
+    move = raw_input("Enter move: \n")
+    column = ord(move[0]) - 97
+    row = int(move[1:])-1
+    while not is_empty(column, row):
+        move = raw_input("Incorrect move. Try again\n")
+        column = ord(move[0]) - 97
+        row = int(move[1:]) - 1
+    board[row][column] = curr_turn
+
+
+
+
 def main():
     global EMPTY
+    global curr_turn
     parse()
     makeBoard(board_size)
     print_board()
+    while True:
+        if curr_turn == PLAYER:
+            rand_move()
+            print_board()
+            curr_turn = OPPONENT
+        if curr_turn == OPPONENT:
+            read_move()
+            print_board()
+            curr_turn = PLAYER
 
 main()
